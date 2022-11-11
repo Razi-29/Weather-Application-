f24127d69657836ac0cd60589115c7a00804ca05
Hillary Jean-Pierre <117764860+Razi-29@users.noreply.github.com>
2022-11-11 15:47:00 -0500

    update

--- weatherapp.js
+++ weatherapp.js
@@ -49,7 +49,8 @@ function showTemp(response) {
     document.querySelector("#humidity").innerhtml = Math.round(response.data.temperature.humidity);
     document.querySelector("#windSpeed").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("h4").innerHTML = response.data.condition.description;
-    document.querySelector("h3").innerHTML = response.data.condition.icon_url;
+    document.querySelector("h3").innerHTML =("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    );
 };
 
 function searchCity(cityInput) {

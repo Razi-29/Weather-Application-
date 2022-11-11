bdb38c89ac89b05567bfe59327938f811ebe0c0e
Hillary Jean-Pierre <117764860+Razi-29@users.noreply.github.com>
2022-11-11 15:32:31 -0500

    update to icon

--- weatherapp.js
+++ weatherapp.js
@@ -49,9 +49,10 @@ function showTemp(response) {
     document.querySelector("#humidity").innerhtml = Math.round(response.data.temperature.humidity);
     document.querySelector("#windSpeed").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("h4").innerHTML = response.data.condition.description;
-    document.querySelector("h3").setAttribute(
-    src= "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/"+response.data.condition.icon+".png"
+    let icon = document.querySelector("h3");
+    icon.innterHTML = ("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
+     icon.setAttribute("alt", response.data.condition.description);
 };
 
 function searchCity(cityInput) {

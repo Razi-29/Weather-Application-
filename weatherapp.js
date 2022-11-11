5599a603a21a7e8a2b9ed14390c7307b2da147ca
Hillary Jean-Pierre <117764860+Razi-29@users.noreply.github.com>
2022-11-11 17:14:56 -0500

    UPDATE

--- weatherapp.js
+++ weatherapp.js
@@ -49,8 +49,7 @@ function showTemp(response) {
     document.querySelector("#humid").innerHTML = Math.round(response.data.temperature.humidity);
     document.querySelector("#windSpeed").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("h4").innerHTML = response.data.condition.description;
-    let iconElement = document.querySelector("h3");
-    iconElement.setAttribute(
+    document.querySelector("h3").setAttribute(
   `src`,
   response.data.condition.icon_url
     );

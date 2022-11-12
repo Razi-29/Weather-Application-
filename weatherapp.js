d53e8051d597db5f4f52f31e867edb3571fd1fd2
Hillary Jean-Pierre <117764860+Razi-29@users.noreply.github.com>
2022-11-11 20:15:04 -0500

    Update to current location

--- weatherapp.js
+++ weatherapp.js
@@ -73,9 +73,8 @@ form.addEventListener("click", search);
 //Current Location
 function showPosition(position) {
     let apiKey = "3a4dft388a1bcaf4e40f706coecb9a01";
-      let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.coords.longitude}
-    &lat=${position.coords.latitude}&key=${apiKey}
-  &units=imperial`;
+      let apiUrl = `https://api.shecodes.io/weather/v1/current?
+    lat=${position.coords.latitude}&lon=${position.coords.longitude}&key=${apiKey}&units=imperial`;
     let latitude = (position.coords.latitude);
     let longitude = (position.coords.longitude);
 

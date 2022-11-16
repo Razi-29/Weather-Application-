dc6303aa4a8f5d7c8c90342b0c4b036172491d41
Hillary Jean-Pierre <117764860+Razi-29@users.noreply.github.com>
2022-11-15 20:47:36 -0500

    update

--- weatherapp.js
+++ weatherapp.js
@@ -56,12 +56,14 @@ function displayForecast(response) {
     
             forecastHTML =
                 forecastHTML + `<div>
+        <button class="forecastButton">
         <h5 class="weather-forecast-date">${forecast.time}</h5>
         <div class="icon" id="icon"> ${getEmojiFromIconCode(forecast[0].condition.icon)};</div>
         <div class="tempNumber" id="forecast">${forecast[1].temperature.day}</div>
         <span class="fahrenheit" id="fahForcast"> &deg <a href="#" id="fahrenheit-link">F</a> |</span>
         <span class="celsius" id="celForecast">&deg <a href="#" id="celsius-link">C</a> </span>
         <div class="weatherD" id="weatherD">${forecast[1].condition.description} </div>
+        </button>
 </div>`;
         }
 });
